a455eed61d3aba5bdc81c76edc874200d9902f09
Piotr Kasprzycki <malelol2001@gmail.com>
2025-04-06 22:12:51 +0200

    Adding paths i modyfication of arena

--- SNAPI monster test/SNAPI monster test/ModEntry.cs
+++ SNAPI monster test/SNAPI monster test/ModEntry.cs
@@ -5,25 +5,78 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Locations;
 using StardewValley.Monsters;
+using xTile.Layers;
+using xTile;
+using System.Reflection;
+using xTile.Dimensions;
+
+// refrection dostanie się do data nawet jakbyś nie powineneś
+// typeof(GameLocation).GetField("seasonOverride")
 
 namespace SNAPI_monster_test
 {
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
     {
-        private bool IsBossLoaded;
+        public static bool IsBossAlive = true;
 
         public override void Entry(IModHelper helper)
         {
-            // Sprawdź czy boss został załadowany
-            helper.Events.GameLoop.SaveLoaded += (s, e) => CheckBossPresence();
-            helper.Events.GameLoop.DayStarted += (s, e) => CheckBossPresence();
 
             // Rejestracja komendy debugującej
             //helper.ConsoleCommands.Add("spawn_boss", "Spawnuje bossa", this.SpawnBossCommand);
             helper.Events.Input.ButtonPressed += this.OnbuttonPressed;
+
+            helper.Events.Player.Warped += PlayerWarpedMine10;
         }
+
+        private void PlayerWarpedMine10(object? sender, WarpedEventArgs e)
+        {
+
+            if (e.NewLocation is MineShaft mineshaft)
+            {
+                if (mineshaft.mineLevel == 10)
+                {
+                    if (IsBossAlive)
+                    {
+                        Game1.player.Position = new Vector2(7 * 64, 6 * 64); // wspórzedne są mnożone przez 64 rozmiar płytki zatem jakbym chciał aby był na płątce 10, 20 to robię 10*64, 20*64
+
+                        bool isBossIsDefended = false;
+
+                        GameLocation location = mineshaft;
+                        SlimeBoss boss = new SlimeBoss("SlimeBoss", new Vector2(26 * 64, 11 * 64), 2);
+
+
+                        location.characters.Add(boss);
+                        this.Monitor.Log("Boss został przyzwany");
+
+                    }
+                    if (mineshaft.mineLevel == 11)
+                    {
+                        IsBossAlive = false;
+                    }
+
+
+                    // mineShaft.ElevatorLightSpot = new Point(3, 3);
+
+                    //// Użyj refleksji tutaj jest właśnie na dostanie się do zmiennych mimo że nie powineneś mógł sie dostać
+                    //Type mineShaftType = typeof(MineShaft);
+                    //MethodInfo setElevatorLitMethod = mineShaftType.GetMethod("setElevatorLit", BindingFlags.NonPublic | BindingFlags.Instance);
+                    // var tak = setElevator.GetCalue(Location)
+
+                    //if (setElevatorLitMethod != null)
+                    //{
+                    //    setElevatorLitMethod.Invoke(mineShaft, null);
+                    //}
+
+
+
+                }
+            }
+        }
+
 
         private void OnbuttonPressed(object? sender, ButtonPressedEventArgs e)
         {
@@ -33,24 +86,8 @@ namespace SNAPI_monster_test
             }
         }
 
-        private void CheckBossPresence()
-        {
-            var monsterData = this.Helper.GameContent.Load<Dictionary<string, string>>("Data/Monsters");
-            IsBossLoaded = monsterData.ContainsKey("SlimeBoss");
-
-            this.Monitor.Log($"Boss loaded: {IsBossLoaded}", LogLevel.Info);
-        }
-
         private void SpawnBossCommand(string command, string[] args)
         {
-            if (!IsBossLoaded)
-            {
-                this.Monitor.Log("Boss nie został poprawnie załadowany!", LogLevel.Error);
-                return;
-            }
-
-
-
             GameLocation location = Game1.player.currentLocation;
 
             SlimeBoss boss = new SlimeBoss("SlimeBoss", new Vector2(Game1.player.Position.X + 64, Game1.player.Position.Y), 2);
@@ -66,9 +103,13 @@ namespace SNAPI_monster_test
 public class SlimeBoss : Monster
 {
     private bool hasSpawnedSlimes = false;
+    private bool hasSpawnedbats = false;
     private int slimesToSpawn = 5;
+    private int batsToSpawn = 3;
     private int spawnTimer = 0;
     private const int SPAWN_INTERVAL = 60; // 60 klatek ≈ 1 sekunda przy 60 FPS
+
+
 
     public SlimeBoss(string name, Vector2 position, int facingDir)
         : base(name, position, facingDir)
@@ -86,7 +127,7 @@ public class SlimeBoss : Monster
         this.Scale = 0.5f;
 
         // Ustawienie prostokąta źródłowego dla pierwszej klatki
-        this.Sprite.SourceRect = new Rectangle(0, 0, frameWidth, frameHeight);
+        this.Sprite.SourceRect = new Microsoft.Xna.Framework.Rectangle(0, 0, frameWidth, frameHeight);
 
         // Tworzenie animacji
         List<FarmerSprite.AnimationFrame> animation = new List<FarmerSprite.AnimationFrame>();
@@ -110,10 +151,11 @@ public class SlimeBoss : Monster
         base.update(time, location);
 
         // Sprawdź, czy boss ma mniej niż 50% HP i jeszcze nie zaczął spawnować slimów
-        if (this.Health <= this.MaxHealth / 2 && !hasSpawnedSlimes)
+        if (this.Health <= this.MaxHealth / 2 && !hasSpawnedSlimes && !hasSpawnedbats)
         {
             // Rozpocznij procedurę spawnowania slimów
             hasSpawnedSlimes = true;
+            hasSpawnedbats = true;
             spawnTimer = 0;
         }
 
@@ -130,6 +172,27 @@ public class SlimeBoss : Monster
                 slimesToSpawn--;
             }
         }
+
+        // Jeśli trwa spawnowanie bat
+        if (hasSpawnedbats && batsToSpawn > 0)
+        {
+            spawnTimer++;
+
+            // Co sekundę (60 klatek) spawnuj nowego bata
+            if (spawnTimer >= SPAWN_INTERVAL)
+            {
+                Spawnbat(location);
+                spawnTimer = 0;
+                batsToSpawn--;
+            }
+        }
+
+        if(this.health.Value <= 0)
+        {
+            SNAPI_monster_test.ModEntry.IsBossAlive = false;
+        }
+
+
     }
 
     private void SpawnSlime(GameLocation location)
@@ -140,6 +203,8 @@ public class SlimeBoss : Monster
             (float)random.Next(-128, 128),
             (float)random.Next(-128, 128)
         );
+
+
 
         Vector2 slimePosition = this.Position + offset;
 
@@ -172,6 +237,46 @@ public class SlimeBoss : Monster
 
         // Efekt dźwiękowy
         Game1.playSound("slime");
+    }
+
+
+    private void Spawnbat(GameLocation location)
+    {
+        // Stwórz nowego slime'a w losowym miejscu wokół bossa
+        Random random = new Random();
+        Vector2 offset = new Vector2(
+            (float)random.Next(-128, 128),
+            (float)random.Next(-128, 128)
+        );
+
+        Vector2 batPosition = this.Position + offset;
+
+        // Tworzenie nowego slime'a
+        // Używamy standardowego Green Slime z gry
+        Bat bat = new Bat(batPosition, 0);
+
+        // Ustawienie właściwości slime'a
+        bat.Health = 50;
+        bat.MaxHealth = 50;
+        bat.Scale = 1; // Nieco mniejszy niż boss
+
+        // Dodanie slime'a do lokacji
+        location.characters.Add(bat);
+
+        // Efekt wizualny pojawienia się slime'a
+        location.temporarySprites.Add(new TemporaryAnimatedSprite(
+            6, // Indeks animacji eksplozji
+            batPosition,
+            Color.LightGreen,
+            8, // Ilość klatek
+            false,
+            100f, // Czas trwania każdej klatki
+            0,
+            -1,
+            -1f,
+            -1,
+            0
+        ));
     }
 
     //// Opcjonalnie: dodaj wizualny wskaźnik kiedy boss zaczyna spawnować slime'y
